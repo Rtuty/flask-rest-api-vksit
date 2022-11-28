8d290bc7b53afa7b257a4ad68b0136609e2c8649
Kudryavtsev Kirill <rtutiui@mail.ru>
2022-11-28 22:09:09 +0300

    add get users

--- src/app/main.py
+++ src/app/main.py
@@ -1,3 +1,18 @@
 from flask import Flask, request
 from classes import *
 
+app = Flask(__name__)
+
+# Получить список пользователей
+@app.route('api/users')
+def GetUsers():
+    result = ''
+    users = []
+    for us in users:
+        user = us.name + ", " + us.age + ", " + us.gender + ", " +us.job + ", " +us.hobby
+        result = result + user
+    return result, 200
+
+
+if __name__ == '__main__':
+    app.run()
