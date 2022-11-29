c752d39515299c43b84d7181bd7d57a732204af3
Kudryavtsev Kirill <rtutiui@mail.ru>
2022-11-30 00:03:33 +0300

    add put method for users

--- src/app/main.py
+++ src/app/main.py
@@ -6,12 +6,14 @@ app = Flask(__name__)
 client = app.test_client()
 
 users_list = [
-    {
+    {   
+        'id': 1,
         'name':'Kirill',
         'surname': 'Kudryavcev',
         'age':19
     },
-    {
+    {   
+        'id': 2,
         'name':'Bob',
         'surname': 'Brown',
         'age':33
@@ -27,6 +29,18 @@ def PostQuery():
     new_one = request.json
     users_list.append(new_one)
     return jsonify(users_list)
+
+
+@app.route('users/put/<int:users_list_id>', methods=['PUT'])
+def PutQuery(users_list_id):
+    item = next((x for x in users_list if x['id'] == users_list_id), None)
+    params = request.json
+
+    if not item:
+        return {'message': 'Ошибка! Объект с указаным идентификатором не найден'}, 400
+    item.update(params)
+
+    return jsonify(item)
 
 
 if __name__ == '__main__':
