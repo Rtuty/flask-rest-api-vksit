f73e90a3171296917b06039b7c60e9cdefcd7756
Kudryavtsev Kirill <rtutiui@mail.ru>
2022-12-03 16:20:40 +0300

    fix post method

--- src/app/main.py
+++ src/app/main.py
@@ -1,12 +1,12 @@
 from flask import Flask, jsonify, request
-#from flask_swagger_ui import get_swaggerui_blueprint
-#from classes import User, Music
+from lib.classes import User
+
 
 app = Flask(__name__)
 
 client = app.test_client()
 
-users_list = [
+users = [
     {   
         'id': 1,
         'name':'Kirill',
@@ -21,7 +21,7 @@ users_list = [
     }
 ]
 
-musics_list = [
+musics = [
     {
         'name': 'smells like teen spirit',
         'author': 'nirvana',
@@ -37,27 +37,60 @@ musics_list = [
 @app.route('/api/get/<string:name>', methods=['GET'])
 def GetQuery(name):
     if name == 'users':
-        return jsonify(users_list), 200
+        return jsonify(users), 200
     if name == 'musics':
-        return jsonify(musics_list), 200
+        return jsonify(musics), 200
+
+#todo
+# @app.route('/api/get/id/<string:name>/<int:id>', methods=['GET'])
+# def GetQuery(name, id):
+#     if name == 'users':
+#         return jsonify(users[id]), 200
+#     if name == 'musics':
+#         return jsonify(users[id]), 200
+
+
+@app.route('/api/createuser', methods=['POST'])
+def PostQuery():
+    if request.method == 'POST':
+        req = request.get_json()
+
+        id = None
+        name = None
+        surname = None
+        age = None
+    
+        if req:
+            if 'id' in req:
+                id = req['id']
+            else:
+                return 'Ошибка! id пользователя отсутствует', 400
+            if 'name' in req:
+                name = req['name']
+            else:
+                return 'Ошибка! имя пользователя отсутствует', 400
+            if 'surname' in req:
+                surname = req['surname']
+            else:
+                return 'Ошибка! фамилия пользователя отсутствует', 400
+            if 'age' in req:
+                age = req['age']
+            else:
+                return 'Ошибка! возраст пользователя отсутствует', 400
+        else: return 'Вы не ввели никаких данных', 404
+
+        user = User(id, name, surname, age)
+        users.append(user)
+        return 'Новый пользователь добавлен', 200
+
+    return 'Неизвестная ошибка', 404
+
     
 
-@app.route('/api/post/<string:name>', methods=['POST'])
-def PostQuery(name):
-    new_one = request.json
-    if name == 'users':
-        users_list.append(new_one)
-        return jsonify(users_list)
-    if name == 'musics':
-        musics_list.append(new_one)
-        return jsonify(musics_list)
 
-    
-
-
-@app.route('/api/put/<int:user_list_id>', methods=['PUT'])
+@app.route('/api/put/users/<int:user_list_id>', methods=['PUT'])
 def update_tutorial(user_list_id):
-    item = next((x for x in users_list if x['id'] == user_list_id), None)
+    item = next((x for x in users if x['id'] == user_list_id), None)
     params = request.json
     if not item:
         return {'message': 'No users with this id'}, 400
@@ -67,14 +100,14 @@ def update_tutorial(user_list_id):
 
 @app.route('/api/delete/<int:user_list_id>', methods=['DELETE'])
 def delete_tutorial(user_list_id):
-    idx, _ = next((x for x in enumerate(users_list)
+    idx, _ = next((x for x in enumerate(users)
                    if x[1]['id'] == user_list_id), (None, None))
 
-    users_list.pop(idx)
+    users.pop(idx)
     return '', 204
 
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port='3005')
